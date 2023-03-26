be94bf14d3cbafa18c1f06fc39a6411632ef678c
mnunezsa <mnunezsa95@gmail.com>
2023-03-26 14:31:45 -0400

    adding JS

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+let initialCards = [
+  {
+    name: "New Orleans",
+    link: "https://unsplash.com/photos/FzPG_1QJouY",
+  },
+  {
+    name: "Boston",
+    link: "https://unsplash.com/photos/VtqqOg_eHTM",
+  },
+  {
+    name: "Chicago",
+    link: "https://unsplash.com/photos/Nyvq2juw4_o",
+  },
+  {
+    name: "Philly",
+    link: "https://unsplash.com/photos/rUMzI6Vn7hU",
+  },
+  {
+    name: "Miami",
+    link: "https://unsplash.com/photos/Um9AkOiIDcU",
+  },
+  {
+    name: "NYC",
+    link: "https://unsplash.com/photos/2f8SXXbP9nY",
+  },
+];
