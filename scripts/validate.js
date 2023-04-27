b339b3fddf2ba1cd83aa78dfd94f6583020891cf
mnunezsa <mnunezsa95@gmail.com>
2023-04-27 17:20:00 -0400

    feature / separate validation to new file

---
+++ scripts/validate.js
@@ -0,0 +1,70 @@
+// Form Validation
+const formElement = document.querySelector(".modal__form");
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  errorElement.classList.add("modal__input_type_error");
+  errorElement.classList.add("modal__input-error_active");
+  errorElement.textContent = errorMessage;
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  errorElement.classList.remove("modal__input_type_error");
+  errorElement.classList.remove("modal__input-error_active");
+  errorElement.textContent = "";
+};
+
+const isValid = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll(".modal__input"));
+  const buttonElement = formElement.querySelector(".modal__save-button");
+  toggleButtonState(inputList, buttonElement);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", () => {
+      isValid(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.disabled = true;
+    buttonElement.classList.add("modal__save-button-inactive");
+  } else {
+    buttonElement.disabled = false;
+    buttonElement.classList.remove("modal__save-button-inactive");
+  }
+};
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll(".modal__form"));
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", function (evt) {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement);
+  });
+};
+
+enableValidation({
+  formElement: ".modal__form",
+  inputElement: ".modal__input",
+  buttonElement: ".modal__save-button",
+  toggleButtonState: "modal__save-button-inactive",
+  errorElement: "modal__input_type_error",
+  errorClass: "modal__input-error_active",
+});
