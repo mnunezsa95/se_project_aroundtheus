584b91b4fc474d3b903fa91d655d9309d69d1482
mnunezsa <mnunezsa95@gmail.com>
2023-06-28 10:27:24 -0400

    reorganized code to make more sense

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,36 +27,24 @@ import {
   previewImageModal,
   cardTemplateElement,
   profileAvatarPeniclIcon,
+  initialCards,
 } from "../utils/constants.js";
 import API from "../components/API.js";
 import { data } from "autoprefixer";
 
 /* ---------------------------------------------------------------------------------------------- */
-/*                                         Form Validator                                         */
+/*                                         Class Instances                                        */
 /* ---------------------------------------------------------------------------------------------- */
-
 const editProfileFormValidator = new FormValidator(config, profileEditModalSelector);
 const addCardFormValidator = new FormValidator(config, cardModalSelector);
 const avatarFromValidator = new FormValidator(config, profileAvatarModalSelector);
-
-editProfileFormValidator.enableValidation();
-addCardFormValidator.enableValidation();
-avatarFromValidator.enableValidation();
-
-/* ---------------------------------------------------------------------------------------------- */
-/*                                      PopupWithForm Classes                                     */
-/* ---------------------------------------------------------------------------------------------- */
-
+//
+const userInfo = new UserInfo(profileTitleSelector, profileDescriptionSelector, profileAvatarSelector);
 const editProfilePopup = new PopupWithForm(profileEditModalSelector, handleProfileFormSubmit);
 const previewImagePopup = new PopupWithImage(previewImageModal);
 const addNewCardPopup = new PopupWithForm(cardModalSelector, handleSubmitCard);
 const deleteImagePopup = new PopupWithConfirm(deleteCardModalSelector);
 const editAvatarPopup = new PopupWithForm(profileAvatarModalSelector, handleProfileAvatarSubmit);
-editAvatarPopup.setEventListeners();
-
-/* ---------------------------------------------------------------------------------------------- */
-/*                                           API Section                                          */
-/* ---------------------------------------------------------------------------------------------- */
 
 const api = new API({
   baseURL: "https://around.nomoreparties.co/v1/cohort-3-en",
@@ -66,34 +54,43 @@ const api = new API({
   },
 });
 
-// //! Do Not Delete
-api.getInitialCards().then((cards) => {
-  const cardListSection = new Section(
-    {
-      items: cards,
-      renderer: (data) => {
-        const newCard = createCard(data);
-        cardListSection.addItem(newCard);
-      },
-    },
-    cardListElement
-  );
-  cardListSection.renderItems();
-});
+/* ---------------------------------------------------------------------------------------------- */
+/*                                         Form Validators                                        */
+/* ---------------------------------------------------------------------------------------------- */
+
+editProfileFormValidator.enableValidation();
+addCardFormValidator.enableValidation();
+avatarFromValidator.enableValidation();
 
 /* ---------------------------------------------------------------------------------------------- */
-/*                                         Profile Classes                                        */
+/*                                           API Promise                                          */
 /* ---------------------------------------------------------------------------------------------- */
+
 let userId;
 
 //! Do Not Delete
-const userInfo = new UserInfo(profileTitleSelector, profileDescriptionSelector, profileAvatarSelector);
-api.getUserInfo().then((userData) => {
-  console.log(userData);
-  userId = userData._id;
-  userInfo.setUserInfo(userData.name, userData.about);
-  userInfo.setProfileAvatar(userData.avatar);
-});
+Promise.all([api.getInitialCards(), api.getUserInfo()])
+  .then(([initialCards, userData]) => {
+    userId = userData._id;
+    userInfo.setUserInfo(userData.name, userData.about);
+    userInfo.setProfileAvatar(userData.avatar);
+    const cardListSection = new Section(
+      {
+        items: initialCards,
+        renderer: (data) => {
+          const newCard = createCard(data);
+          cardListSection.addItem(newCard);
+        },
+      },
+      cardListElement
+    );
+    cardListSection.renderItems();
+  })
+  .catch(() => (err) => console.log(err));
+
+/* ---------------------------------------------------------------------------------------------- */
+/*                                        Profile Functions                                       */
+/* ---------------------------------------------------------------------------------------------- */
 
 //! Do Not Delete
 function openProfilePopup() {
@@ -114,7 +111,17 @@ function handleProfileFormSubmit({ title, description }) {
   editProfilePopup.close();
 }
 
+/* ---------------------------------------------------------------------------------------------- */
+/*                                   Profile Set Event Listeners                                  */
+/* ---------------------------------------------------------------------------------------------- */
+
 profileEditButton.addEventListener("click", openProfilePopup);
+
+profileAvatarPeniclIcon.addEventListener("click", () => {
+  avatarFromValidator.toggleButtonState();
+  editAvatarPopup.open();
+  editAvatarPopup.setLoading(false, "Save");
+});
 
 /* ---------------------------------------------------------------------------------------------- */
 /*                                         Card Functions                                         */
@@ -130,18 +137,15 @@ function handleProfileAvatarSubmit(link) {
   });
 }
 
-//!Do Not Delete
-profileAvatarPeniclIcon.addEventListener("click", () => {
-  avatarFromValidator.toggleButtonState();
-  editAvatarPopup.open();
-  editAvatarPopup.setLoading(false, "Save");
-});
-
-addNewCardButton.addEventListener("click", () => {
-  addCardFormValidator.toggleButtonState();
-  addNewCardPopup.open();
-  addNewCardPopup.setLoading(false, "Create");
-});
+//! Do not delete
+function handleSubmitCard({ title, url }) {
+  api.addCard(title, url).then((card) => {
+    const newCard = createCard(card);
+    cardListElement.prepend(newCard);
+  });
+  addNewCardPopup.setLoading(true);
+  addNewCardPopup.close();
+}
 
 //! Do not delete
 function createCard(data) {
@@ -170,33 +174,11 @@ function createCard(data) {
   return newCard.generateCard();
 }
 
-//! Do not delete
-function handleSubmitCard({ title, url }) {
-  api.addCard(title, url).then((card) => {
-    const newCard = createCard(card);
-    cardListElement.prepend(newCard);
-  });
-  addNewCardPopup.setLoading(true);
-  addNewCardPopup.close();
-}
-
-//! Do Not Delete
-api
-  .getAppInfo()
-  .then(([initialCards, userData]) => {
-    userId = userData._id;
-    userInfo.setProfileInfo(userData.name, userData.about);
-    userInfo.setProfileAvatar(userData.avatar);
-    const cardListSection = new Section(
-      {
-        items: initialCards,
-        renderer: (data) => {
-          const newCard = createCard(data);
-          cardListSection.addItem(newCard);
-        },
-      },
-      cardListElement
-    );
-    cardListSection.renderItems();
-  })
-  .catch(() => (err) => console.log(err));
+/* ---------------------------------------------------------------------------------------------- */
+/*                                    Card Set Event Listeners                                    */
+/* ---------------------------------------------------------------------------------------------- */
+addNewCardButton.addEventListener("click", () => {
+  addCardFormValidator.toggleButtonState();
+  addNewCardPopup.open();
+  addNewCardPopup.setLoading(false, "Create");
+});
